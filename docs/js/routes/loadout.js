f5fb3955d0747ff76e5fbac52623912abc45bc5c
gavinlohnes <gavin.lohnes@gmail.com>
2026-05-05 03:57:26 -0600

    Add files via upload

---
+++ docs/js/routes/loadout.js
@@ -0,0 +1,8 @@
+export function renderLoadout(app) {
+    app.innerHTML = `
+        <div class="panel">
+            <div class="section-title">Loadout</div>
+            <p>Your system configuration.</p>
+        </div>
+    `;
+}
