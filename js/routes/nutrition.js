ebe5bfa10aa967b6e1fcb0a14f708a4f77960a6e
gavinlohnes <gavin.lohnes@gmail.com>
2026-05-05 03:56:30 -0600

    Delete js directory

--- js/routes/nutrition.js
+++
@@ -1,8 +0,0 @@
-export function renderNutrition(app) {
-    app.innerHTML = `
-        <div class="panel">
-            <div class="section-title">Nutrition</div>
-            <p>Anchor meals and macros.</p>
-        </div>
-    `;
-}
--- js/routes/sync.js
+++
@@ -1,8 +0,0 @@
-export function renderSync(app) {
-    app.innerHTML = `
-        <div class="panel">
-            <div class="section-title">Sync Ritual</div>
-            <p>Weekly review + preload.</p>
-        </div>
-    `;
-}
