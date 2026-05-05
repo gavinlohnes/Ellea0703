dd621b8c9e883d3688a27c2272fc7a0211253695
gavinlohnes <gavin.lohnes@gmail.com>
2026-05-05 03:38:32 -0600

    Add files via upload

---
+++ BEYOND-OS/js/routes/sync.js
@@ -0,0 +1,8 @@
+export function renderSync(app) {
+    app.innerHTML = `
+        <div class="panel">
+            <div class="section-title">Sync Ritual</div>
+            <p>Weekly review + preload.</p>
+        </div>
+    `;
+}
