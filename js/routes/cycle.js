59cae434447fe0db83875fec93afbfa11f1d1561
gavinlohnes <gavin.lohnes@gmail.com>
2026-05-05 03:41:59 -0600

    Add files via upload

---
+++ js/routes/cycle.js
@@ -0,0 +1,8 @@
+export function renderCycle(app) {
+    app.innerHTML = `
+        <div class="panel">
+            <div class="section-title">Cycle</div>
+            <p>Your daily + weekly rhythm.</p>
+        </div>
+    `;
+}
