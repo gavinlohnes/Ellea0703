d099f22860fe40603a34adf94fc595307f33cf4f
gavinlohnes <gavin.lohnes@gmail.com>
2026-05-05 03:33:56 -0600

    Add files via upload

---
+++ BEYOND-OS - Copy/js/routes/hud.js
@@ -0,0 +1,8 @@
+export function renderHUD(app) {
+    app.innerHTML = `
+        <div class="panel">
+            <div class="section-title">HUD Overview</div>
+            <p>Welcome to BEYOND OS.</p>
+        </div>
+    `;
+}
---
+++ BEYOND-OS - Copy/js/routes/nutrition.js
@@ -0,0 +1,8 @@
+export function renderNutrition(app) {
+    app.innerHTML = `
+        <div class="panel">
+            <div class="section-title">Nutrition</div>
+            <p>Anchor meals and macros.</p>
+        </div>
+    `;
+}
