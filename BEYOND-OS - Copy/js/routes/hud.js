ca754319b79aa4605ae632cb23c51528ed314860
gavinlohnes <gavin.lohnes@gmail.com>
2026-05-05 03:38:09 -0600

    Delete BEYOND-OS - Copy directory

--- BEYOND-OS - Copy/js/routes/hud.js
+++
@@ -1,8 +0,0 @@
-export function renderHUD(app) {
-    app.innerHTML = `
-        <div class="panel">
-            <div class="section-title">HUD Overview</div>
-            <p>Welcome to BEYOND OS.</p>
-        </div>
-    `;
-}
