78246c9aa7343f73ee87504228e1c63d6d1800b1
gavinlohnes <gavin.lohnes@gmail.com>
2026-05-07 19:56:17 -0500

    Delete docs directory

--- docs/js/routes/cycle.js
+++
@@ -1,8 +0,0 @@
-export function renderCycle(app) {
-    app.innerHTML = `
-        <div class="panel">
-            <div class="section-title">Cycle</div>
-            <p>Your daily + weekly rhythm.</p>
-        </div>
-    `;
-}
--- docs/js/routes/loadout.js
+++
@@ -1,8 +0,0 @@
-export function renderLoadout(app) {
-    app.innerHTML = `
-        <div class="panel">
-            <div class="section-title">Loadout</div>
-            <p>Your system configuration.</p>
-        </div>
-    `;
-}
